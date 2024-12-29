1926c8d233b57ccb58ceaa31ce4799fbbf044f7f
Vlad13356 <butiscavlad.vb@gmail.com>
2024-12-29 16:19:01 +0200

    Clasa Profesor

--- ProjectOOP/ProjectOOP/Profesor.cs
+++ ProjectOOP/ProjectOOP/Profesor.cs
@@ -1,6 +1,38 @@
-﻿namespace ProjectOOP;
+﻿using System.Threading.Channels;
 
-public class Profesor
+namespace ProjectOOP;
+public class Profesor:Utilizator
 {
+    public Profesor(string numarMatricol, string numePrenume, string email, string parola) :
+        base(numarMatricol,numePrenume,email,parola)
+    {
+        
+    }
     
+    
+    public override string showMenu()
+    {
+
+        string opt;
+        bool meniu=true;
+        do
+        {
+            Console.WriteLine("Alegeti o optiune: ");
+            opt = Console.ReadLine();
+            switch (opt)
+            {
+                case "0": Console.WriteLine("0.Iesire");
+                    return "Ati iesit din meniu"; 
+                case "1": Console.WriteLine("1.Deschidere sesiune"); break;
+                case "2": Console.WriteLine("2.Inchidere sesiune"); break;
+                case "3": Console.WriteLine("3.Notare studenti"); break;
+                case "4": Console.WriteLine("4.Raspuns reclamatii"); break;
+                case "5": Console.WriteLine("5.Modificare nota"); break;
+            }
+            
+            
+        } while (meniu);
+
+        return "Meniul sa terminat ";
+    }
 }
--- ProjectOOP/ProjectOOP/Program.cs
+++ ProjectOOP/ProjectOOP/Program.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// See https://aka.ms/new-console-template for more information
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        
-    }
-}
+Console.WriteLine("Hello, World!");
